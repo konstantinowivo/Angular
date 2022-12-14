6b4f022b6166eb57bd9ebd9e745e26c88791d64a
Ivo K <105252341+konstantinowivo@users.noreply.github.com>
2022-12-14 11:29:41 -0300

    Add files via upload

---
+++ Clase 1 - Introducción/main.js
@@ -0,0 +1,52 @@
+// Temario curso Angular
+
+// * Que es angular?
+// * Instalación de Angular y software necesario
+// * Creación de primera aplicación
+// * Estructura de aplicaciones
+// * Creación de componentes
+// * Binding
+// * Directivas de utilidad
+// * Forms
+// * Comunicación
+// * Servicios
+// * Routing
+// * HTTP y autenticación
+// * Despliegue de aplicaciones
+
+
+// Que es Angular?
+
+// Angular es un framework para crear aplicaciones web SPA(Simple Page Aplication)
+// La caracteristicas de estas páginas es que son de una sóla página, al contrario de un sitio web
+// que se compone de muchas páginas. Esto no quiere decir que sea más básica ni menos funcional
+
+
+// Quien creó Angular?
+// Google
+
+
+
+// Que caracteristicas tiene?
+
+// * Separa frontend de backend de la aplicación
+// * simplifica el código
+// * sigue el patrón MVC (Modelo Vista Controlador)
+// * Basado en componentes
+// * Es de código abierto
+
+
+// Que lenguaje utiliza?
+
+// Typescript (Basado en JS, pero con mayor orientación a los objetos)
+
+// Por que utilizar Angular?
+
+// * El desarrollo de aplicaciones es rápido y la navegación también mediante AJAX(asincronía)
+// * Es modular (reutilizable) gracias a MVC, cosas que desarrollamos para una aplicacion
+// podemos usarlas en otras
+// * Fácil mantenimiento
+// * Es multiplataforma
+// * Futuro estable
+// * Gran soporte de herramientas
+// * Creciente demanda
---
+++ Clase 2 - Instalación/main.js
@@ -0,0 +1,11 @@
+// Instalación Software necesario
+
+// * Node JS. (Entorno JS en tiempo de ejecución del lado del servidor) (ok)
+// * NPM (Node package manager) - administrador de paquetes de Node.js
+// * Angular CLI(Command Line Interface)
+// * Visual Studio Code
+// * Git (Adicional) - No es imprescindible
+
+
+
+// Creación de primera aplicación
